78417ac5342ba5f366132654c580378e2ad87142
hunie25 <tranhuyen08052006@gmail.com>
2025-07-23 16:22:07 +0700

    bai1.py

--- src/bai1.py
+++ src/bai1.py
@@ -5,7 +5,7 @@ if thang in [1, 3, 5, 7, 8, 10, 12]:
 elif thang in [4, 6, 9, 11]:
     print ("30")
 else:
-    if nam % 4 == 0 and nam % 100 != 0:
+    if (nam % 4 == 0 and nam % 100 != 0) or (nam % 400 == 0):
         print ("29")
     else:
         print ("28")
